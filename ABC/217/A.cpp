0d9efd276fb8f5c5da589abb2182917bbf8ae96b
Kadowaki0312.res@gmail.com <kadowaki0312.res@gmail.com>
2021-09-11 20:31:12 +0900

    update

--- ABC/217/A.cpp
+++ ABC/217/A.cpp
@@ -6,4 +6,15 @@ using namespace std;
 
 int main()
 {
+    string S, T;
+    cin >> S >> T;
+
+    if (S < T)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
 }
