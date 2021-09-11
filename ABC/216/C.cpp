35f5ed231179ea2d6e78e27d1e887190894837b5
Kadowaki0312.res@gmail.com <kadowaki0312.res@gmail.com>
2021-09-11 22:44:45 +0900

    update

--- ABC/216/C.cpp
+++ ABC/216/C.cpp
@@ -2,4 +2,31 @@
 #include <vector>
 #include <cmath>
 #include <stdio.h>
+#include <algorithm>
+#include <string>
 using namespace std;
+
+int main()
+{
+    long long N;
+    cin >> N;
+
+    string ans;
+
+    while (N > 0)
+    {
+        if (N % 2 == 1)
+        {
+            ans += "A";
+            N -= 1;
+        }
+        else
+        {
+            ans += "B";
+            N /= 2;
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+    cout << ans << endl;
+}
