2ad9ad2692449f2b62235723feed57878b9bfad7
Kadowaki0312.res@gmail.com <kadowaki0312.res@gmail.com>
2021-09-18 21:53:05 +0900

    C分からん

---
+++ ABC/219/C.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <stdio.h>
+#include <typeinfo>
+using namespace std;
+
+int main()
+{
+    string X;
+    int N;
+
+    cin >> X;
+    cin >> N;
+    vector<string> S(N);
+    for (int i = 0; i < S.size(); i++)
+    {
+        cin >> S[i];
+    }
+}
