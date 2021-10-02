ed160e5c2f6311df53c71b3b138c455094ca14ee
MizutoKadowaki0312 <kadowaki0312.res@gmail.com>
2021-10-02 21:07:23 +0900

    回答写した

--- ABC/220/A.cpp
+++ ABC/220/A.cpp
@@ -6,40 +6,15 @@ int main()
     int A;
     int B;
     int C;
-
     cin >> A >> B >> C;
 
-    int ans = 0;
-    int count = 0;
+    int Y = B / C * C;
 
-    for (int i = 0; i < (B - A + 1); i++)
+    if (A <= Y)
     {
-        ans = C * i;
-        cout << i << " " << ans << endl;
-        if (ans < A)
-        {
-            continue;
-        }
-        if (ans >= B)
-        {
-            break;
-        }
-        if (A <= ans && ans <= B)
-        {
-            count += 1;
-        }
-        if (!(A <= ans && ans <= B))
-        {
-            count += 0;
-        }
+        cout << Y << endl;
     }
-
-    if (1 <= count)
-    {
-        cout << ans << endl;
-    }
-
-    if (count == 0)
+    else
     {
         cout << -1 << endl;
     }
